9f72ae8b56a878cceb145e89c7747a609b43077a
Goce Mitevski <goce.mitevski@gmail.com>
2016-04-22 23:20:05 +0200

    Initial commit

---
+++ js/render.js
@@ -0,0 +1,26 @@
+// Земи JSON и претвори го во објекти
+var json_data = (function () {
+    var json = null;
+    $.ajax({
+        'async': false,
+        'global': false,
+        'url': "json/dizajni.json",
+        'dataType': "json",
+        'success': function (data) {
+            json = data;
+        }
+    });
+    return json;
+})();
+
+var dizajni = json_data.dizajni;
+
+// Рандомизирај листа на дизајни
+// http://stackoverflow.com/a/18650169/3190066
+dizajni.sort(function() {
+  return 0.8 - Math.random();
+});
+
+$(dizajni).each(function () {
+    $("<div class='dizajn-wrap'><a href='img/" + this.ime + "." + this.file_ext + "'><img class='dizajn-thumb img-responsive' src='img/" + this.ime + ".png'></a><h2>" + this.zasluga + "</h2></div>").insertBefore('.pechatnici');
+});
