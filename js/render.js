8dcd6637217287d5fd9b9820a28ed1d969b62b89
Goce Mitevski <goce.mitevski@gmail.com>
2016-04-26 16:50:23 +0200

    Enabled adding specific names for thumbnails, different than the one's of source files for print.

--- js/render.js
+++ js/render.js
@@ -1,5 +1,5 @@
 // Земи JSON и претвори го во објекти
-$.get("json/dizajni.json").then(function(data) {
+$.get("json/dizajni.json").then(function (data) {
     var dizajni = data['dizajni'];
 
     // Рандомизирај листа на дизајни
@@ -9,14 +9,22 @@ $.get("json/dizajni.json").then(function(data) {
     });
 
     // Изгенерирај DOM елементи
-    $(dizajni).each(function() {
+    $(dizajni).each(function () {
+
+        // Провери дали има посебно назначено име за thumbnail. Ако нема, земи го името на изворниот фајл за печатење.
+        if (this.hasOwnProperty('ime_png') === true) {
+            var img_png = this.ime_png;
+        } else {
+            var img_png = this.ime;
+        }
+
         var thumb_el = $(
-            "<div class='dizajn-wrap'>" +
+                "<div class='dizajn-wrap'>" +
                 "<a href='img/" + this.ime + "." + this.file_ext + "'>" +
-                    "<img class='dizajn-thumb img-responsive' alt='" + this.ime + "' src='img/" + this.ime + ".png'>" +
+                "<img class='dizajn-thumb img-responsive' alt='" + this.ime + "' src='img/" + img_png + ".png'>" +
                 "</a>" +
                 "<h2>" + this.zasluga + "</h2>" +
-            "</div>");
+                "</div>");
         thumb_el.insertBefore('.pechatnici');
     });
-})
+});
